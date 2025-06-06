fb2a001c3c23025855ec00319b0e49d493a78db4
jjw0609 <jjw_0609@naver.com>
2025-06-06 22:38:46 +0900

    DB정보 설정

---
+++ oauth/src/main/java/com/example/oauth/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.example.oauth.member.controller;
+
+import com.example.oauth.member.service.MemberService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
