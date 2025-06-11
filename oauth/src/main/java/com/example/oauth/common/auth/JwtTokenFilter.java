703cabd1d651bf620b272afe6a538d0c09391581
jjw0609 <jjw_0609@naver.com>
2025-06-11 21:08:03 +0900

    회원가입

---
+++ oauth/src/main/java/com/example/oauth/common/auth/JwtTokenFilter.java
@@ -0,0 +1,6 @@
+package com.example.oauth.common.auth;
+
+
+public class JwtTokenFilter {
+        
+}
--- oauth/src/main/java/com/example/oauth/member/controller/MemberController.java
+++ oauth/src/main/java/com/example/oauth/member/controller/MemberController.java
@@ -1,13 +1,38 @@
 package com.example.oauth.member.controller;
 
+import com.example.oauth.member.domain.Member;
+import com.example.oauth.member.dto.MemberCreateDto;
+import com.example.oauth.member.dto.MemberLoginDto;
 import com.example.oauth.member.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/member")
 public class MemberController {
+
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> memberCreate(@RequestBody MemberCreateDto memberCreateDto) {
+        Member member = memberService.create(memberCreateDto);
+
+        return new ResponseEntity<>(member.getId(), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/doLogin")
+    public ResponseEntity<?> doLogin(@RequestBody MemberLoginDto memberLoginDto) {
+        //email, password 검증
+        Member member = memberService.login(memberLoginDto);
+
+        //일치할 경우 jwt accesstoken 생성
     }
 }
