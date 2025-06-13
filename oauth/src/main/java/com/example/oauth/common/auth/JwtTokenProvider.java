5645ef8fdfa76bca7a2d85aecc9c84465b41eb6d
jjw0609 <jjw_0609@naver.com>
2025-06-13 21:52:20 +0900

    토큰 생성

--- oauth/src/main/java/com/example/oauth/common/auth/JwtTokenProvider.java
+++ oauth/src/main/java/com/example/oauth/common/auth/JwtTokenProvider.java
@@ -1,6 +1,23 @@
 package com.example.oauth.common.auth;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+
+@Component
 public class JwtTokenProvider {
 
+    private final String secretKey;
+    private final int expiration;
+    private Key encodedSecretKey;
+
+
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration}") int expiration) {
+        this.secretKey = secretKey;
+        this.expiration = expiration;
+        this.encodedSecretKey = new SecretKeySpec(java.util.Base64.getDecoder().decode(secretKey), SignatureAlgorithm.HS512.getJcaName);
+    }
 }
