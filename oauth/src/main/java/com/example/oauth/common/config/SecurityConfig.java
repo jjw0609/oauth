1dc3730a49fa475331b08c6febede062f616172f
jjw0609 <jjw_0609@naver.com>
2025-06-18 21:03:49 +0900

    토큰 검증

--- oauth/src/main/java/com/example/oauth/common/config/SecurityConfig.java
+++ oauth/src/main/java/com/example/oauth/common/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.oauth.common.config;
 
+import com.example.oauth.common.auth.JwtTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -16,6 +18,12 @@ import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    public SecurityConfig(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
 
     @Bean
     public PasswordEncoder makePassword() {
@@ -35,6 +43,8 @@ public class SecurityConfig {
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 //특정 URL패턴에 대해서는 인증처리(Authentication 객체 생성) 제외
                 .authorizeHttpRequests(a -> a.requestMatchers("/member/create", "/member/doLogin"))
+                //UsernamePasswordAuthenticationFilter 클래스에서 form 로그인 인증을 처리
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
