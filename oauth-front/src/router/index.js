61c38faa47fc903f215089009a65baa234a1dfdd
jjw0609 <jjw_0609@naver.com>
2025-07-10 21:03:49 +0900

    구글 로그인 - 인가코드

--- oauth-front/src/router/index.js
+++ oauth-front/src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import MemberCreate from "@/components/MemberCreate.vue";
 import MemberLogin from "@/components/MemberLogin.vue";
+import GoogleRedirect from "@/components/GoogleRedirect.vue";
 
 const routes = [
     {
@@ -10,6 +11,10 @@ const routes = [
     {
         path: '/member/login',
         component: MemberLogin
+    },
+    {
+        path: '/oauth/google/redirect',
+        component: GoogleRedirect
     }
 ]
 
